f30507a9f47ecb0ba9d683e44d673f1224cbb831
Olimjon <olimjonsn230@gmail.com>
2022-06-29 17:03:22 +0500

    string_length

--- string_length01_test.dart
+++ string_length01_test.dart
@@ -1,7 +1,7 @@
 import 'package:test/test.dart';
 import '../bin/string_length01.dart';
 
-void main(List<String> args) {
+void main() {
   test('string_length01', () {
     expect(func("code"), equals("code".length));
   });
