20cd46a40c99c17b172c62e2cf734d87fcc2fbd4
olimjonsn230 <olimjonsn230@gmail.com>
2022-07-02 15:43:50 +0500

    done

--- string_length06_test.dart
+++ string_length06_test.dart
@@ -3,9 +3,6 @@ import '../bin/string_length06.dart';
 
 void main() {
   test('string_length06', () {
-    expect(
-        func("code", "school"),
-        equals(
-            "code".length > "school".length ? "school".length : "code".length));
+    expect(func("code", "school"), equals("code"));
   });
 }
